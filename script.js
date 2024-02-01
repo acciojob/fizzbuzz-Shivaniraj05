34483f415189f1ac5f942ef93c34ed41d70fce6c
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-02-01 23:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function isMultipleOfThree(num){
+    return num%3 === 0;
+}
+
+function isMultipleOfFive(num){
+    return num%5 === 0;
+}
+
+function fizzBuss(){
+    let output="";
+    for(let i=1;i<=20;i++){
+        if(isMultipleOfThree(i) && isMultipleOfFive(i)){
+            output += "FizzBuzz\n";
+        }
+
+        else if(isMultipleOfThree(i)){
+            output += "Fizz\n";
+        }
+
+        else if(isMultipleOfFive(i)){
+            output += "Buzz\n";
+        }
+
+        else{
+            output += i+"\n";
+        }
+    }
+
+    return output;
+
+}
+alert(fizzBuss());
